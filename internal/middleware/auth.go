14e71e72bca4d6b172e6ac474c552419b3c138f9
MI-Bachtiar <bachtiar@madeindonesia.com>
2022-06-27 21:32:01 +0700

    Middleware: fixing auth

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -2,8 +2,14 @@ package middleware
 
 import (
 	"api_course/internal/user"
+	"context"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+)
+
+const (
+	bearer = "Bearer "
 )
 
 func Authentication(userService *user.UserService) gin.HandlerFunc {
@@ -15,6 +21,24 @@ func Authentication(userService *user.UserService) gin.HandlerFunc {
 			})
 			c.Abort()
 		}
+
+		if !strings.HasPrefix(authHeader, bearer) {
+			c.JSON(401, gin.H{
+				"message": "unauthorize",
+			})
+			c.Abort()
+		}
+
+		auths := strings.Split(authHeader, " ")
+		data, err := userService.DecriptJWT(auths[1])
+		if err != nil {
+			c.JSON(401, gin.H{
+				"message": "unauthorize",
+			})
+			c.Abort()
+		}
+		ctxUserID := context.WithValue(c.Request.Context(), "user_id", data["user_id"])
+		c.Request = c.Request.WithContext(ctxUserID)
 		c.Next()
 	}
 }
