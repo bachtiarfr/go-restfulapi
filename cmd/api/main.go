24b146dfc2b85d52069fce89374ceae578a65b38
MI-Bachtiar <bachtiar@madeindonesia.com>
2022-06-27 21:21:18 +0700

    Fixing routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -21,17 +21,14 @@ func main() {
 	userService := user.NewUserService(db)
 
 	route.GET("/exercise/:id", exerciseService.GetExercise)
+	route.POST("/register", userService.RegisterUser)
+	route.POST("/login", userService.LoginUser)
 	
-	//exercise
 	route.POST("/exercises", middleware.Authentication(userService), exerciseService.CreateExercise)
 	route.GET("/exercises/:exerciseId", middleware.Authentication(userService), exerciseService.GetExercise)
 	route.GET("/exercises/:exerciseId/score", middleware.Authentication(userService), exerciseService.GetUserScore)
 	route.POST("/exercises/:exerciseId/questions", middleware.Authentication(userService), exerciseService.CreateQuestions)
 	route.POST("/exercises/:exerciseId/questions/:questionId/answer", middleware.Authentication(userService), exerciseService.CreateAnswer)
-	
-	//user
-	route.POST("/register", userService.RegisterUser)
-	route.POST("/login", userService.LoginUser)
 
 	route.Run(":8080")
 }
